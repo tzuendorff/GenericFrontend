f8dcad14e4e5cf33a876187731ed979c1cd72513
Tristan Zündorff <tzuendorff@gmail.com>
2025-07-22 19:09:18 +0200

    feat: Adds eslint-plugin-unicorn for some basic good linting.

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 
 export default defineConfig([
+  eslintPluginUnicorn.configs.recommended,
+
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
