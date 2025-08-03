1ee8035f2815354b6fbdc482fa27387e9f1d2eb8
Tristan Zündorff <tzuendorff@gmail.com>
2025-08-03 19:26:58 +0200

    feat: adds filtering to each column

--- src/components/OrdersTable.jsx
+++ src/components/OrdersTable.jsx
@@ -4,6 +4,8 @@ import { Column } from "primereact/column";
 import { Chip } from "primereact/chip";
 import { ProgressSpinner } from "primereact/progressspinner";
 import { Button } from "primereact/button";
+import { FilterMatchMode } from "primereact/api";
+import { TriStateCheckbox } from "primereact/tristatecheckbox";
 
 import { fetchOrders, deleteOrder } from "../lib/HttpService"; // Import the fetchOrders function
 import "./OrdersTable.css";
@@ -15,6 +17,14 @@ const OrdersTable = () => {
   const [orders, setOrders] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
+
+  const [filters] = useState({
+    global: { value: undefined, matchMode: FilterMatchMode.CONTAINS },
+    id: { value: undefined, matchMode: FilterMatchMode.CONTAINS },
+    customerFirstName: { value: undefined, matchMode: FilterMatchMode.CONTAINS },
+    customerLastName: { value: undefined, matchMode: FilterMatchMode.CONTAINS },
+    approved: { value: undefined, matchMode: FilterMatchMode.EQUALS },
+  });
 
   useEffect(() => {
     const loadOrders = async () => {
@@ -32,19 +42,18 @@ const OrdersTable = () => {
   }, []);
 
   const onClickDeleteOrder = async (orderId) => {
-      try {
-        await deleteOrder(orderId);
-        console.log(orderId)
-        
-        setOrders(orders.filter((order) => order.id !== orderId));
-        
-      } catch (error_) {
-        setError("There was an error fetching the orders.");
-        console.error("Error fetching data: " + error_);
-      } finally {
-        setLoading(false);
-      }
-  }
+    try {
+      await deleteOrder(orderId);
+      console.log(orderId);
+
+      setOrders(orders.filter((order) => order.id !== orderId));
+    } catch (error_) {
+      setError("There was an error fetching the orders.");
+      console.error("Error fetching data: " + error_);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   if (loading) {
     return (
@@ -59,24 +68,27 @@ const OrdersTable = () => {
     return <div className="text-red-500 text-center mt-4 text-xl">{error}</div>;
   }
 
-  const approvedBodyTemplate = (rowData) => {
-    return <Chip label={rowData.approved ? "Yes" : "No"} style={{backgroundColor: rowData.approved ? "var(--blue-200)" : "var(--red-200)"}} />;
-  }
-
   const deleteButton = (rowData) => {
-    return <Button label="Delete" severity="danger" onClick={() => onClickDeleteOrder(rowData.id)}/>
-  }
+    return <Button label="Delete" severity="danger" onClick={() => onClickDeleteOrder(rowData.id)} />;
+  };
 
+  const verifiedRowFilterTemplate = (options) => {
+    return <TriStateCheckbox value={options.value} onChange={(e) => options.filterApplyCallback(e.value)} />;
+  };
+
+  const approvedBodyTemplate = (rowData) => {
+    return <Chip label={rowData.approved ? "Yes" : "No"} style={{ backgroundColor: rowData.approved ? "var(--blue-200)" : "var(--red-200)" }} />;
+  };
   return (
     <div className="card m-4">
       <h1 className="text-3xl font-bold text-center mb-4">Customer Orders</h1>
       {orders && orders.length > 0 ? (
-        <DataTable value={orders} stripedRows className="orders-datatable">
-          <Column sortable field="id" header="Order ID" />
-          <Column sortable field="customerFirstName" header="First Name" />
-          <Column sortable field="customerLastName" header="Last Name" />
-          <Column sortable field="approved" header="Approved" body={approvedBodyTemplate} />
-          <Column header="Delete" body={deleteButton}/>
+        <DataTable value={orders} stripedRows className="orders-datatable" filters={filters} filterDisplay="row">
+          <Column header="Order ID"   field="id"                filter sortable />
+          <Column header="First Name" field="customerFirstName" filter sortable />
+          <Column header="Last Name"  field="customerLastName"  filter sortable />
+          <Column header="Approved"   field="approved"          filter sortable filterElement={verifiedRowFilterTemplate}  body={approvedBodyTemplate} dataType="boolean" />
+          <Column header="Delete" body={deleteButton} />
         </DataTable>
       ) : (
         <div className="text-center">No orders found.</div>
